e6903ff3537ff71fa6c8ae44e0a5881e00aec3a4
oclancy <oclancy@gmail.com>
2018-12-18 12:13:47 +0000

    fix build

--- dist/mycore/esm5/mycore.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/**
- * Generated bundle index. Do not edit.
- */
-export { ServiceState, SignalrClientService, DataService, AuthService, AuthGuard, CoreService, CoreComponent, MyCoreModule } from './public_api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibXljb3JlLmpzIiwic291cmNlUm9vdCI6Im5nOi8vbXljb3JlLyIsInNvdXJjZXMiOlsibXljb3JlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFJQSxrSUFBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljX2FwaSc7XG4iXX0=
--- dist/mycore/lib/service-state.d.ts
+++
@@ -1,4 +0,0 @@
-export declare enum ServiceState {
-    Unititialised = 0,
-    Intialised = 1
-}
